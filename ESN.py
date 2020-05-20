88c9105f200df6a088cec93167958efe2a189aaf
Dimi3-admin <dimitri.lereverend@gmail.com>
2020-05-20 15:11:58 +0200

    Changes in update and Simulate, problem of sometime null maximum eigenvalue (nilpotent matrix?) to fix!

--- ESN.py
+++ ESN.py
@@ -29,7 +29,7 @@ class ESN:
         self.number_output = number_output
         self.N = number_neurons  #How many neurons in our reservoir
         self.x = np.zeros((self.N))   #Intern state of the reservoir. Initialisation might change
-        self.y = np.zeros((self.N))
+        self.y = np.zeros((self.number_output))
         self.n_iter = 0
 
         self.W = np.zeros((self.N,self.N))  #The internal weigth matrix
@@ -40,29 +40,47 @@ class ESN:
                 if is_connected:
                     self.W[i,j] = 2*random.random()-1   #Uniformly between -1 and 1, maybe to change
         eigenvalue = max(abs(np.linalg.eig(self.W)[0]))
+        print(eigenvalue,self.W)
         self.W *= spectral_radius/eigenvalue            #We normalize the weight matrix to get the desired spectral radius.
         self.W_in = 2*np.random.rand(self.N,self.number_input) - 1  #We initialise between -1 and 1 uniformly, maybe to change
-        self.W_out = 2*np.random.rand(self.number_output,self.number_output+self.N + self.number_input) - 1
-        self.W_back = 2*np.random.rand(self.N,self.number_input) - 1 #The Feedback matrix
+        self.W_out = 2*np.random.rand(self.number_output, self.number_output+self.N + self.number_input) - 1
+        self.W_back = 2*np.random.rand(self.N,self.number_output) - 1 #The Feedback matrix
 
     def update(self,input = []):
         '''
+        Advance the process by 1 step
         '''
         if input == []:
             input = np.zeros((self.number_input))
         self.x = sigmoid(np.dot(self.W_in, input) + np.dot(self.W, self.x) + np.dot(self.W_back, self.y))       #Sigmoid use
-        self.y = tanh(np.dot(self.W_out,np.concatenate(np.concatenate(input,self.x,self.y))))                   #We use tanh for the output.
+        tab1 = np.concatenate((input,self.x))
+        tab2 = np.concatenate((tab1,self.y))
+        self.y = tanh(np.dot(self.W_out,tab2))                   #We use tanh for the output.
+        self.n_iter +=1
 
 
     def simulation(self, nb_iter,initial_inputs):
-        pass
+        '''
+        Simulates the behaviour of the ESN given a starting sequence (initial_inputs) for nb_iter iterations.
+        '''
+        for input in initial_inputs:
+            self.update(input)
+        self.n_iter = 0     #We reset the iterations, and should have an initialised reservoir.
+        while self.n_iter <= nb_iter:
+            self.update()
 
-    def save(self):
+
+    def save(self,name):
         pass
 
     @classmethod
-    def load(self):
+    def load(self,name):
         pass
 
-test= ESN(10,0.1,100,50,0.7)
+test= ESN(number_neurons = 10,proba_connexion = 0.1,number_input = 100, number_output = 1,spectral_radius = 0.7)
 print(max(abs(np.linalg.eig(test.W)[0]))) #Check wether the spectral radius is respected.
+print(test.y)
+test.update()
+print(test.y)
+test.update()
+print(test.y)
