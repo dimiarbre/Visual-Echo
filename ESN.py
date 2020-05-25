00d6c2096db9e68560bd206eb4754317388ee760
Dimi3-admin <dimitri.lereverend@gmail.com>
2020-05-25 15:03:05 +0200

    Add of leaky integrator, test of normal evolution to redo since strange behaviours

--- ESN.py
+++ ESN.py
@@ -8,47 +8,44 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def tanh(x):
-    expo = np.exp(-2*x)
-    return (1-expo)/(1+expo)
-
-def reciprocal_tanh(x):
-    return 0.5 * (np.ln(1 + x) - np.ln(1 - x))
+    return np.tanh(x)
 
 class ESN:
     '''
     Notes that this is a specific Echo State Network for training purpose, without the maximum features.
     It may ultimately be a basic one for spatialisation purpose.
     '''
-    def __init__(self,number_neurons, proba_connexion,number_input,number_output,spectral_radius):
+    def __init__(self,number_neurons, proba_connexion,number_input,number_output,spectral_radius,leak_rate):
         '''
         Creates an instance of ESN given some parameters
         :parameters:
             - number_neurons : The number of neurons in the reservoir
             - proba_connexion : the probability of connecting to another neuron (or the density of connexion in the reservoir)
-            - number_input : How many input neurons
-            - number_output : How many output neurons
+            - number_input : How many input neurons.
+            - number_output : How many output neurons.
             - spectral_radius : the desired spectral radius, depending on how lastong we want the memory to be.
+            - leak_rate: The leak_rate on every update, symbolize the amount of information kept/lost.
         '''
         self.number_input = number_input
         self.number_output = number_output
         self.N = number_neurons  #How many neurons in our reservoir
-        self.x = np.zeros((self.N))   #Intern state of the reservoir. Initialisation might change
+        self.x = 2*np.random.rand((self.N))-1   #Internal state of the reservoir. Initialisation might change
         self.y = np.zeros((self.number_output))
         self.n_iter = 0
-
+        self.leak_rate = leak_rate
         self.W = np.zeros((self.N,self.N))  #The internal weigth matrix
         #The loop will connect the reservoir with a certain probability.
         for i in range(self.N):
             for j in range(self.N):
                 is_connected = (random.random()<proba_connexion)
-                if is_connected:
+                if is_connected and i!=j:
                     self.W[i,j] = 2*random.random()-1   #Uniformly between -1 and 1, maybe to change
         eigenvalue = max(abs(np.linalg.eig(self.W)[0]))
         if eigenvalue == 0.0:
             raise Exception("Null Maximum Eigenvalue")
         self.W *= spectral_radius/eigenvalue            #We normalize the weight matrix to get the desired spectral radius.
         self.W_in = 2*np.random.rand(self.N,self.number_input) - 1  #We initialise between -1 and 1 uniformly, maybe to change
-        self.W_out = 2*np.random.rand(self.number_output, self.number_output+self.N + self.number_input) - 1
+        self.W_out = 2*np.random.rand(self.number_output, self.number_output + self.N + self.number_input) - 1
         self.W_back = 2*np.random.rand(self.N,self.number_output) - 1 #The Feedback matrix
 
     def update(self,input = []):
@@ -65,11 +62,13 @@ class ESN:
         matrix1 = np.dot(self.W_in, input)
         matrix2 = np.dot(self.W, self.x)
         matrix3 = np.dot(self.W_back, self.y)
-        self.x = sigmoid( matrix1 + matrix2 + matrix3)       #Sigmoid use -> should change the function for discrete time
+
+        self.x =(1-self.leak_rate)*self.x + self.leak_rate * tanh(matrix1 + matrix2 + matrix3)
         if (np.array_equal(test,self.x)):
             print("Etat identique",self.n_iter)
+            print(self.x)
         tab1 = np.concatenate((input,self.x,self.y))
-        self.y = tanh(np.dot(self.W_out,tab1))                   #We use tanh for the output.
+        self.y = np.dot(self.W_out,tab1)                     #We use tanh for the output.
         self.n_iter +=1
 
 
@@ -106,9 +105,6 @@ class ESN:
             self.W_out[j] = newWeights
             print(self.W_out)
 
-        pass
-
-
     def save(self,name):
         pass
 
@@ -120,20 +116,25 @@ class ESN:
 #Mackey glass function import.
 file = open("mgdata.dat.txt")
 mackey_glass = list(map(lambda x : [float(x.split(" ")[1].split("\n")[0])] ,file.readlines()))
-total_len = len(mackey_glass)
+file.close()
+total_len = 3*len(mackey_glass)
+mackey_glass = 3*mackey_glass
+print(total_len)
 
 def compare_MG(esn,starting_iteration):
-
-    plt.plot([i for i in range(starting_iteration,total_len)], [mackey_glass[i] for i in range(starting_iteration,total_len)],label = "Mackey Glass")
-    plt.plot([i for i in range(starting_iteration,total_len)],esn.simulation(total_len-starting_iteration,[mackey_glass[i] for i in range(starting_iteration)]),label = "ESN response")
+    simu = esn.simulation(total_len-starting_iteration,[mackey_glass[i] for i in range(starting_iteration)])
+#    print(simu)
+    #plt.plot([i for i in range(starting_iteration,total_len)], [mackey_glass[i] for i in range(starting_iteration,total_len)],label = "Mackey Glass")
+    plt.plot([i for i in range(starting_iteration,total_len)],simu,label = "ESN response")
 
     plt.legend()
     plt.show()
 
 
-test= ESN(number_neurons = 400, proba_connexion = 0.2, number_input = 1, number_output = 1, spectral_radius = 0.5)
+test= ESN(number_neurons = 400, proba_connexion = 0.1, number_input = 1, number_output = 1, spectral_radius = 0.7,leak_rate = 0.9)
 print(max(abs(np.linalg.eig(test.W)[0]))) #Check wether the spectral radius is respected.
-test.train([mackey_glass[i] for i in range(1000)],[mackey_glass[i] for i in range(1000)],100)
+
+#test.train(mackey_glass,mackey_glass,1000)
 
 '''
 test.update(mackey_glass[0])
@@ -141,4 +142,6 @@ test.update(mackey_glass[1])
 for i in range(100):
     test.update()
 '''
+
 compare_MG(test,100)
+print(test.x)
